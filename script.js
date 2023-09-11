81af8e175b688801107218964dd7a884908f321b
Prabhu2222 <120732312+Prabhu2222@users.noreply.github.com>
2023-09-11 10:44:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let newArr=[];
+let obj={};
+arr.forEach(word=>{
+	let regexp=/\b(the|an|a)\b/g;
+	let newWord=word.replace(regexp,"");
+	obj[newWord]=word;
+	newArr.push(newWord);
+});
+newArr.sort();
+let ans=[];
+for(let i of newArr){
+	ans.push(obj[i]);
+}
+console.log(ans);
 
